9844066657a3f61436c21969fefa60add41d71e0
chirag gaur <chiragwalker@gmail.com>
2023-08-07 23:20:35 +0530

    done

---
+++ block-BNaaaj/area.js
@@ -0,0 +1,15 @@
+// area formulas
+
+//square
+
+module.exports = {
+  square: function square(a) {
+    return a * a;
+  },
+  rectangle: function rectangle(l, w) {
+    return l * w;
+  },
+  circle: function rectangle(r) {
+    return Math.PI * 2 * r;
+  },
+};
