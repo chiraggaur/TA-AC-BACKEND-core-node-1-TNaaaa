21381c1df6baf8ba42082035f4a335c028dd5936
chirag gaur <chiragwalker@gmail.com>
2023-08-08 14:46:26 +0530

    updated  assign json formatt

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -14,7 +14,7 @@ function handlerequest(req, res) {
     res.writeHead(202, { "content-Type": "Text/html" });
     res.end("<h2>this is all about NodeJS</h2>");
   } else if (req.method === "POST" && req.url === "/about") {
-    res.writeHead(202, { "content-Type": "Text/html" });
+    res.writeHead(202, { "content-Type": "application/json" });
     res.end("{message: this is a post request}");
   } else {
     res.writeHead(404, { "content-Type": "Text/html" });
